a4c00cfe09ace96798fdc86b72643f0dac3fe19a
Yass525 <yassine.trabelsi1@esprit.tn>
2022-03-07 13:48:12 +0100

    update file structures ready to use

---
+++ server/services/user/app.js
@@ -0,0 +1,57 @@
+const express = require('express')
+const morgan = require ('morgan')
+const creteError = require ('http-errors')
+const rateLimit = require('express-rate-limit')
+const helmet = require('helmet')
+
+require('dotenv').config({ path: '../../.env' })
+
+require('../../db')
+
+const profileRoute = require('./Routes/profile.route')
+const paymentRoute = require('./Routes/payment.route')
+
+const limiter = rateLimit({
+    max:5,
+    windowMs: 1 * 60 * 1000,
+    standardHeaders: true,
+	legacyHeaders: false, 
+})
+
+const app = express()
+app.use(morgan('dev'))
+app.use(express.json())
+app.use(express.urlencoded({extended:true}))
+app.use(limiter)
+app.use(helmet.xssFilter());
+app.use(helmet.hsts());
+
+app.get('/', async(req,res,next)=>{
+     res.send("basic user")
+})
+
+app.use('/profile', profileRoute)
+app.use('/payment', paymentRoute)
+
+app.use(async(req,res,next)=>{
+    // const error = new Error ("Not found")
+    // error.status = 404
+    // next(error)
+    next(creteError.NotFound())
+})
+
+app.use((err,req,res,next)=>{
+    res.status(err.status || 500)
+    res.send({
+        error :{
+            status: err.status || 500,
+            message: err.message,
+        }
+    })
+})
+
+ const PORT =  3001
+
+ app.listen(PORT,()=>{
+     console.log("server running on port "+PORT)
+ })
