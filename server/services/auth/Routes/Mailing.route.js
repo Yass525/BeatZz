ae75cebfe3fddfca212449c5484686e976c1aacf
Yass525 <yassine.trabelsi1@esprit.tn>
2022-04-02 17:19:50 +0100

    adding mail confirmation

--- server/services/auth/Routes/Mailing.route.js
+++
@@ -1,7 +0,0 @@
-const express = require('express')
-const router = express.Router()
-const MailingService = require('../helpers/Mailing.service')
-
-router.get('/confirmation/:token', MailingService.confirmEmail)
-
-module.exports = router;
